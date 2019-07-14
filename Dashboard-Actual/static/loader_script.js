82dcceaa3b7caf29f83bfc7172db34e3357e7abf
Jeffrey Boisvert <jboisvert377@gmail.com>
2019-07-14 16:48:23 -0400

    Added loadingMessages and getRandomMessage to loader_script.js along with logic to display the loader icon and remove the button after the button has been clicked.

--- Dashboard-Actual/static/loader_script.js
+++ Dashboard-Actual/static/loader_script.js
@@ -1,7 +1,35 @@
+//Make this in the future load from a text file
+function getRandomMessage(){
+
+    return "Just scrapping the website. Should only take a few moments!"
+
+}
+
+//Function used to generate random texts about the loading process
+function loadingMessages(){
+
+    //An infinite loop to ensure messages are always displayed
+    while (true){
+
+        let message = getRandomMessage();
+        $('#viewboardContainer').html(message);
+
+    }
+
+
+}
+
 //When document is loaded
 $(document).ready(function() {
     
-    //Make loader appear
+    //Used to make the loader icon appear and make enter button disappear
+    document.getElementById('dashboardEnter').addEventListener('click', function(){
+
+        $('#loader').css('display': 'block'); 
+        $('#viewboard').css('display': 'none');
+
+
+    })
 
 
 });
